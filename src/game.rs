dc986358106f89713d47c7f23e2c4a0a251c0c93
Prince Wilson <maxcell.wilson@gmail.com>
2022-03-09 22:53:43 -0500

    Adds creating game logic and showing the board (#14)

---
+++ src/game.rs
@@ -0,0 +1,156 @@
+use itertools::Itertools;
+use serenity::{
+    builder::{CreateActionRow, CreateButton},
+    model::prelude::*,
+};
+use sqlx::{Executor, FromRow, PgPool, Row};
+
+use crate::word_bank::sample_word_bank;
+
+#[derive(Debug, sqlx::FromRow)]
+pub struct Card {
+    pub text: String,
+    pub is_touched: bool,
+    pub card_type: CardType,
+}
+
+impl Card {
+    pub fn build_button(&self) -> CreateButton {
+        let mut button: CreateButton = CreateButton::default();
+        button.label(self.text.clone());
+        button.custom_id(self.text.clone());
+        button
+    }
+
+    // These represent the cards initial states for the
+    // rest of the players (not the spymasters)
+    pub fn build_untouched_button(&self) -> CreateButton {
+        let mut my_button = self.build_button();
+        my_button.style(interactions::message_component::ButtonStyle::Secondary);
+        my_button
+    }
+
+    // These represent the behind-the-scenes values for
+    // the spymasters
+    pub fn build_touched_button(&self) -> CreateButton {
+        let mut my_button = self.build_button();
+        my_button.style(interactions::message_component::ButtonStyle::Secondary);
+
+        let emoji = match self.card_type {
+            CardType::Red => String::from("🔴"),
+            CardType::Blue => String::from("🔵"),
+            CardType::Neutral => String::from("🟤"),
+            CardType::Assassin => String::from("☠️"),
+        };
+
+        my_button.emoji(serenity::model::channel::ReactionType::Unicode(emoji));
+        my_button.disabled(true);
+        my_button
+    }
+}
+
+pub struct Game {
+    id: i32,
+}
+
+#[derive(Debug)]
+pub struct Board {
+    pub cards: Vec<Card>,
+}
+
+#[derive(Debug, sqlx::Type)]
+#[sqlx(type_name = "card_type", rename_all = "lowercase")]
+pub enum CardType {
+    Red,
+    Blue,
+    Neutral,
+    Assassin,
+}
+
+pub async fn create_game(db_connection: &PgPool, guild_id: String) {
+    // Add our server into the list of servers that have played codenames
+    sqlx::query!(
+        "INSERT INTO new_server (guild_id) values ($1) ON conflict (guild_id) DO nothing;",
+        guild_id
+    )
+    .execute(db_connection)
+    .await
+    .expect("failed to invoke db query");
+
+    // Find an active game for that server
+    let active_game = sqlx::query!("
+        SELECT id from new_game WHERE
+            (game_state = 'created' OR game_state = 'playing') AND guild_id = ($1)
+        ORDER BY created_at DESC LIMIT 1", guild_id)
+    .fetch_one(db_connection)
+    .await;
+
+    // If there is an active game, early return
+    if let Ok(_) = active_game { return (); }
+
+    // Otherwise, there is no active game and create a new one
+    let new_game = sqlx::query!(
+        "INSERT INTO new_game (guild_id) values ($1) RETURNING id",
+        guild_id
+    )
+    .fetch_one(db_connection)
+    .await
+    .expect("Failed to invoke db query");
+
+    let select_words = sample_word_bank(25);
+
+    for (position, word) in select_words.iter().enumerate() {
+        sqlx::query!(
+            "INSERT INTO game_words (word_id,game_id,card_type) VALUES ($1, $2, $3)",
+            word,
+            new_game.id,
+            match position as u32 {
+                0..=7 => CardType::Red,
+                8..=16 => CardType::Blue,
+                17..=23 => CardType::Neutral,
+                24 => CardType::Assassin,
+                _ => panic!("Position inserted was incorrect"),
+            } as CardType
+        )
+        .execute(db_connection)
+        .await
+        .expect("Failed to invoke db query");
+    }
+}
+
+impl Board {
+    pub fn create_list(cards: Vec<Card>) -> Board {
+        Board { cards }
+    }
+
+    pub fn build(&self) -> Vec<CreateActionRow> {
+        let mut action_rows: Vec<CreateActionRow> = vec![];
+        for word_chunks in &self.cards.iter().chunks(5) {
+            let mut new_action_row = CreateActionRow::default();
+
+            word_chunks.for_each(|word| {
+                new_action_row.add_button(word.build_untouched_button());
+            });
+
+            action_rows.push(new_action_row);
+        }
+
+        action_rows
+    }
+
+    // todo!("make sure to not let this stay here");
+    pub fn build_seen(&self) -> Vec<CreateActionRow> {
+        let mut action_rows: Vec<CreateActionRow> = vec![];
+        for word_chunks in &self.cards.iter().chunks(5) {
+            let mut new_action_row = CreateActionRow::default();
+
+            word_chunks.for_each(|word| {
+                new_action_row.add_button(word.build_touched_button());
+            });
+
+            action_rows.push(new_action_row);
+        }
+
+        action_rows
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,180 @@
-use std::{env};
+use std::{collections::HashMap, convert::TryFrom, env, sync::Arc};
 
-mod dictionary;
+mod game;
 
-use dictionary::{Board};
-
+use game::{create_game, Board};
 use serenity::{
     async_trait,
     framework::standard::{
-        help_commands,
-        macros::{command, group, help},
-        Args,
-        CommandResult,
-        StandardFramework,
+        macros::{command, group},
+        Args, CommandResult, StandardFramework,
     },
     http::Http,
+    model::interactions::application_command::ApplicationCommandInteractionDataOptionValue,
     model::prelude::*,
     prelude::*,
 };
+use tokio::sync::RwLock;
+
+mod word_bank;
+
+struct Game;
+
+impl TypeMapKey for Game {
+    type Value = Arc<RwLock<HashMap<u64, Board>>>;
+}
+
+struct Db;
+impl TypeMapKey for Db {
+    type Value = Arc<sqlx::PgPool>;
+}
+
+struct CodenameCommand {
+    data: interactions::application_command::ApplicationCommandInteraction,
+    option: interactions::application_command::ApplicationCommandInteractionDataOptionValue,
+}
+
+enum SubCommand {
+    Show,
+}
+
+impl CodenameCommand {
+    fn content(&self) -> String {
+        if let ApplicationCommandInteractionDataOptionValue::String(selection) = self.option.clone()
+        {
+            match selection.as_str() {
+                "show" => "Here's the secret board. Don't speak to anyone about it!".to_string(),
+                "create" => "New Game Created!".to_string(),
+                _ => "Command not implemented".to_string(),
+            }
+        } else {
+            "Invalid Option sent to the command".to_string()
+        }
+    }
+}
+
+impl TryFrom<interactions::application_command::ApplicationCommandInteraction> for CodenameCommand {
+    type Error = ();
+
+    fn try_from(
+        value: interactions::application_command::ApplicationCommandInteraction,
+    ) -> Result<Self, Self::Error> {
+        if value.data.name.as_str() == "codename" {
+            let option = value
+                .data
+                .options
+                .get(0)
+                .expect("Expect receive option")
+                .clone()
+                .resolved
+                .expect("Failed to use provided option");
+
+            return Ok(CodenameCommand {
+                data: value,
+                option,
+            });
+        }
+
+        return Err(());
+    }
+}
 
 struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
     async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
-        if let Interaction::ApplicationCommand(command) = interaction {
-            let content = match command.data.name.as_str() {
-                "codename" => "Hey, I'm alive!".to_string(),
-                _ => "Sorry not implemented".to_string()
-            };
 
-        if let Err(why) = command
-            .create_interaction_response(&ctx.http, |response| {
-                response
-                    .kind(InteractionResponseType::ChannelMessageWithSource)
-                    .interaction_response_data(|message| 
-                        message.content(content)
-                        .flags(interactions::InteractionApplicationCommandCallbackDataFlags::EPHEMERAL))
-            }).await
-            {
-                println!("Cannot respond to slash command: {}", why);
+        // Establish a DB connection with the interaction
+        let database_connection = {
+            let arc = ctx.data.read().await;
+
+            arc.get::<Db>()
+                .expect("Failed to get connection to database")
+                .clone()
+        };
+
+        if let Some(cmd) = interaction.application_command() {
+            let option = cmd.clone().data.options.get(0).expect("").clone().resolved.unwrap();
+            if let ApplicationCommandInteractionDataOptionValue::String(selection) = option {
+                match selection.as_str() {
+                    // Reveal the board to the users
+                    // TODO: Show the currently revealed board state vs. the secret board state
+                    "show" => {
+                        let guild_id = cmd.clone().guild_id.unwrap().as_u64().to_string().clone();
+                        let board =  tokio::spawn(async move {
+                            let game = sqlx::query!(
+                                "SELECT id from new_game WHERE guild_id = $1 AND game_state = 'created'
+                                ORDER BY created_at DESC LIMIT 1", guild_id)
+                            .fetch_one(&*database_connection)
+                            .await
+                            .unwrap();
+
+                            let cards = sqlx::query_as!(game::Card, r#"SELECT word_id as text,is_touched,card_type 
+                                as "card_type!: _" from game_words WHERE game_id = $1"#, game.id)
+                            .fetch_all(&*database_connection)
+                            .await
+                            .unwrap();
+
+                            Board { cards }
+                        }).await
+                        .unwrap();
+
+                        if let Err(why) = cmd.clone()
+                            .create_interaction_response(&ctx.http, |response| {
+                                response
+                                    .kind(InteractionResponseType::ChannelMessageWithSource)
+                                    .interaction_response_data(|message| {
+                                         message.components(|c| {
+                                                c.set_action_rows(
+                                                    board.build_seen()
+                                                )
+                                            })
+                                            .flags(interactions::InteractionApplicationCommandCallbackDataFlags::EPHEMERAL);
+                                            message
+                                    })}).await {
+                                        println!("Cannot respond to slash command: {}", why);
+                                    }
+                    }
+                    "create" => {
+                        if let Err(why) = cmd
+                            .clone()
+                            .create_interaction_response(&ctx.http, |response| {
+                                response
+                                    .kind(InteractionResponseType::ChannelMessageWithSource)
+                                    .interaction_response_data(|message| {
+                                        tokio::spawn(async move {
+                                            let guild_id = cmd.guild_id.unwrap();
+                                            create_game(
+                                                &*database_connection,
+                                                guild_id.as_u64().to_string().clone(),
+                                            )
+                                            .await;
+                                        });
+                                        message.content("Game created")
+                                    })
+                            })
+                            .await
+                        {
+                            println!("Cannot respond to slash command: {}", why);
+                        }
+                    }
+                    _ => {
+                        if let Err(why) = cmd
+                            .clone()
+                            .create_interaction_response(&ctx.http, |response| {
+                                response
+                                    .kind(InteractionResponseType::ChannelMessageWithSource)
+                                    .interaction_response_data(|message| {
+                                        message.content("Failed to recognize command")
+                                    })
+                            })
+                            .await
+                        {
+                            println!("Cannot respond to slash command: {}", why);
+                        }
+                    }
+                }
             }
         }
     }
@@ -46,20 +182,31 @@ impl EventHandler for Handler {
     async fn ready(&self, ctx: Context, ready: Ready) {
         println!("{} is connected!", ready.user.name);
 
-        // Test Guild ID - 741467935939231819
-
         // Slash Command for the guild
         GuildId(741467935939231819)
-        .create_application_command(&ctx.http, |command| {
-            command.name("codename").description("A way to show the board")
-        })
-        .await;
+            .create_application_command(&ctx.http, |command| {
+                command
+                    .name("codename")
+                    .description("Commands for Codenames the game")
+                    .create_option(|option| {
+                        option.name("command")
+                .description("Select the commands you'd like to use")
+                .kind(interactions::application_command::ApplicationCommandOptionType::String)
+                .add_string_choice(
+                    "Start a new game",
+                    "create"
+                )
+                .add_string_choice(
+                    "Shows the board",
+                    "show"
+                )
+                .required(true)
+                    })
+            })
+            .await
+            .unwrap();
     }
 }
-
-#[group("collector")]
-#[commands(challenge, show)]
-struct Collector;
 
 #[tokio::main]
 async fn main() {
@@ -76,9 +223,23 @@ async fn main() {
 
     let framework = StandardFramework::new()
         .configure(|c| {
-            c.with_whitespace(true).on_mention(Some(bot_id)).prefix("~").delimiters(vec![", ", ","])
-        })
-        .group(&COLLECTOR_GROUP);
+            c.with_whitespace(true)
+                .on_mention(Some(bot_id))
+                .prefix("~")
+                .delimiters(vec![", ", ","])
+        });
+
+    let database_url = std::env::var("DATABASE_URL")
+        .expect("Make sure to add the DATABASE_URL to the environment variables");
+
+    let database = sqlx::postgres::PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url)
+        .await
+        .expect("Test if we made teh connection");
+
+    // ? Why did we need to use expect here as opposed to question mark?
+    // Is it due to the impl future
 
     let mut client = Client::builder(&token)
         .application_id(904223099552149514)
@@ -86,116 +247,17 @@ async fn main() {
         .framework(framework)
         .await
         .expect("Err creating client");
+    {
+        let mut data = client.data.write().await;
+        data.insert::<Game>(Arc::new(RwLock::new(HashMap::default())));
+    }
+
+    {
+        let mut data = client.data.write().await;
+        data.insert::<Db>(Arc::new(database));
+    }
 
     if let Err(why) = client.start().await {
         println!("Client error: {:?}", why);
     }
-}
-
-#[command]
-async fn challenge(ctx: &Context, msg: &Message, _: Args) -> CommandResult {
-    let msg = msg
-        .channel_id
-        .send_message(&ctx.http, |m| {
-            m.content( "Let's start a new game");
-            m.components(|c| {
-                c.set_action_rows(Board::create_list().build())
-            });
-            m
-        })
-        .await;
-
-        if let Err(why) = msg {
-            println!("Error sending message: {:?}", why);
-        }
-
-    Ok(())
-}
-
-#[command]
-async fn show(ctx: &Context, msg: &Message, _: Args) -> CommandResult {
-    //let _ = msg.reply(ctx, "Let's start a new game").await;
-    let msg = msg
-        .channel_id
-        .send_message(&ctx.http, |m| {
-            m.content( "Let's start a new game");
-            m.components(|c| {
-                c.create_action_row(|r| {
-                    r.create_button(|b| {
-                        b.label("beef");
-                        b.style(interactions::message_component::ButtonStyle::Primary);
-                        b.custom_id("123123");
-                        b
-                    });
-                    r.create_button(|b| {
-                        b.label("steak");
-                        b.style(interactions::message_component::ButtonStyle::Primary);
-                        b.custom_id("12123");
-                        b
-                    });
-                    r.create_button(|b| {
-                        b.label("ham");
-                        b.style(interactions::message_component::ButtonStyle::Primary);
-                        b.custom_id("123");
-                        b
-                    });
-                    r.create_button(|b| {
-                        b.label("chicken");
-                        b.style(interactions::message_component::ButtonStyle::Primary);
-                        b.custom_id("12");
-                        b
-                    });
-                    r.create_button(|b| {
-                        b.label("cow");
-                        b.style(interactions::message_component::ButtonStyle::Primary);
-                        b.custom_id("s2");
-                        b
-                    });
-                    r
-                });
-
-                c.create_action_row(|r| {
-                    r.create_button(|b| {
-                        b.label("beef");
-                        b.style(interactions::message_component::ButtonStyle::Primary);
-                        b.custom_id("4");
-                        b
-                    });
-                    r.create_button(|b| {
-                        b.label("steak");
-                        b.style(interactions::message_component::ButtonStyle::Primary);
-                        b.custom_id("5");
-                        b
-                    });
-                    r.create_button(|b| {
-                        b.label("ham");
-                        b.style(interactions::message_component::ButtonStyle::Primary);
-                        b.custom_id("6");
-                        b
-                    });
-                    r.create_button(|b| {
-                        b.label("chicken");
-                        b.style(interactions::message_component::ButtonStyle::Primary);
-                        b.custom_id("7");
-                        b
-                    });
-                    r.create_button(|b| {
-                        b.label("cow");
-                        b.style(interactions::message_component::ButtonStyle::Primary);
-                        b.custom_id("8");
-                        b
-                    });
-                    r
-                });
-                c
-            });
-            m
-        })
-        .await;
-
-        if let Err(why) = msg {
-            println!("Error sending message: {:?}", why);
-        }
-
-    Ok(())
 }
